e040a1dc8444591ce084bda4fa96459f0088a9cf
Edilson do Nascimento <edilsonjoper@gmail.com>
2019-04-26 14:35:47 -0300

    02 - Variáveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,33 @@
 var titulo = document.querySelector(".container");
 titulo.textContent = "Maria Aparecida Nutricionista";
+
+var trPaciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = trPaciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = trPaciente.querySelector('.info-altura'); 
+var altura = tdAltura.textContent;
+
+var tdImc = trPaciente.querySelector('.info-imc');
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <=0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = 'peso inválido';
+ }
+ 
+ if(altura <= 0 || altura >= 3.00) {
+   alturaEhValida = false;
+   tdImc.textContent = 'altura inválida';
+ }
+
+ console.log(pesoEhValido);
+ console.log(alturaEhValida);
+ 
+ if(pesoEhValido && alturaEhValida) {
+    var imc = peso / ( altura * altura);
+    tdImc.textContent = imc; 
+ }
