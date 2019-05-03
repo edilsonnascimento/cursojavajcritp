ca0e4eef1c61c4eb23318b9e5a64d10848c9867f
Edilson do Nascimento <edilsonjoper@gmail.com>
2019-05-03 09:39:55 -0300

    04 - Eventos, Formulários e Criando Elementos

--- js/principal.js
+++ js/principal.js
@@ -35,3 +35,47 @@ for(var i = 0; i < pacientes.length; i++){
       tdImc.textContent = imc.toFixed(2);
    }         
 }
+
+
+var botaoAdiciona = document.querySelector("#adicionar-paciente");
+
+botaoAdiciona.addEventListener("click", function(event){
+   //desativa comportamento padrão do form 
+   event.preventDefault();
+   //captura formulário.
+   var form = document.querySelector("#form-adiciona");   
+   //capitura valores do form
+   var nome = form.nome.value;
+   var peso = form.peso.value;
+   var altura = form.altura.value;
+   var gordura = form.gordura.value;
+
+   //cria linha
+   var pacienteTr = document.createElement("tr");
+   
+   //cria celula
+   var nomeTd = document.createElement("td");
+   //atribui valor do campo
+   nomeTd.textContent = nome;
+   //adicona celcula na linha;
+   pacienteTr.appendChild(nomeTd);
+
+   var pesoTd = document.createElement("td");
+   pesoTd.textContent = peso;
+   pacienteTr.appendChild(pesoTd);
+
+   var alturaTd = document.createElement("td");
+   alturaTd.textContent = altura;
+   pacienteTr.appendChild(alturaTd);
+
+   var gorduraTd = document.createElement("td");
+   gorduraTd.textContent = gordura;
+   pacienteTr.appendChild(gorduraTd);
+
+   //captura tabela
+   var tabela = document.querySelector("#tabela-pacientes");
+   //adiciona linha preenchida.
+   tabela.appendChild(pacienteTr);
+
+
+});
